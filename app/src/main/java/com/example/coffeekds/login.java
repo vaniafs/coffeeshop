577842c9ae7b88fd354cd90833606ffa4917a824
Cia <zazaskiaa@gmail.com>
2023-02-25 07:38:27 +0700

    zj

---
+++ app/src/main/java/com/example/coffeekds/login.java
@@ -0,0 +1,43 @@
+package com.example.coffeekds;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends Activity {
+
+    EditText username;
+    EditText password;
+    EditText txtUsername;
+    EditText txtPassword;
+    Button login_btn;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.txtUsername);
+        password = (EditText) findViewById(R.id.txtPassword);
+        login_btn = (Button) findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+                if (username.equalsIgnoreCase("admin") &&
+                        password.equalsIgnoreCase("admin")) {
+
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Gagal Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
